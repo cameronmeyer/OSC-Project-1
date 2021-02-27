c0d6d0ba8e7c163f048fe9d46ce868a8e3c2a6a1
Cameron <cameron.meyer02@gmail.com>
2021-02-27 15:42:18 -0600

    Some Basic Framework Setup

--- Project1.cpp
+++ Project1.cpp
@@ -1,9 +1,33 @@
+#include <stack>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <stdio.h>
 #include <iostream>
+using namespace std;
+
+const string WHITESPACE = "\n\r\t\f\v";
+const int start_address = 0;
+const int start_system_code = 1000;
+const int memory_size = 2000;
 
 int main()
 {
-    std::string name;
-    std::cout << "What is your name?" << std::endl;
-    std::cin >> name;
-    std::cout << "Hello, " << name << "!" << std::endl;
+    
+}
+
+int read(int address)
+{
+    return 0;
+}
+
+int write(int address, int data)
+{
+    return 0;
+}
+
+string trim(const string& s)
+{
+    size_t end = s.find_last_not_of(WHITESPACE);
+    return (end == string::npos) ? "" : s.substr(0, end + 1);
 }
