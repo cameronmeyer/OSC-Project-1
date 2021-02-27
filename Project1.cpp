667e735d8305874370e5d725ce648165af35bfcf
Cameron <cameron.meyer02@gmail.com>
2021-02-27 14:52:37 -0600

    Populating VS Files

---
+++ Project1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::string name;
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!" << std::endl;
+}
